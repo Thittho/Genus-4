afec1b7d6f23a3467a747ad6e8802e6d2071dca0
Thittho <bouchetthomas@orange.fr>
2023-07-21 12:07:07 +0200

    Additions to spec

--- magma/Verifications/Proof_HSOP.m
+++ magma/Verifications/Proof_HSOP.m
@@ -1,4 +1,13 @@
-import "~/Genus-4/magma/tools.m": 
+function CoefficientMatrix(f)
+	P := Parent(f);
+	R<a00, a01, a02, a03, a10, a11, a12, a13, a20, a21, a22, a23, a30, a31, a32, a33, X> := CoefficientRing(P);
+
+	m := Degree(Evaluate(f, [P.1, P.2, 1, 2]));	
+	n := Degree(Evaluate(f, [1, 2, P.3, P.4]));
+	M := [[R!MonomialCoefficient(f, (P.2)^i*(P.1)^(m-i)*(P.4)^j*(P.3)^(n-j)) : i in [0..m]] : j in [0..n]];
+	return Matrix(M);
+end function; 
+
 function InvariantsHSOP(f, d)
 	// Covariants
 	// Degree 2
